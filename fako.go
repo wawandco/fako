5254a0d00a1d9078c92f50e8566e972a47070911
Antonio Pagano <apagano@wawand.co>
2015-11-12 12:48:56 -0500

    [firstcommit] adding first commit for fako

---
+++ fako.go
@@ -0,0 +1,61 @@
+package fako
+
+import (
+	"reflect"
+
+	"github.com/wawandco/fako/Godeps/_workspace/src/github.com/icrowley/fake"
+)
+
+var typeMapping = map[string]func() string{
+	"name":     fake.FullName,
+	"email":    fake.EmailAddress,
+	"phone":    fake.Phone,
+	"username": fake.UserName,
+	"password": fake.SimplePassword,
+	"address":  fake.StreetAddress,
+}
+
+func Fill(strukt interface{}) {
+	fillWithDetails(strukt, []string{}, []string{})
+}
+
+func FillOnly(strukt interface{}, fields ...string) {
+	fillWithDetails(strukt, fields, []string{})
+}
+
+func FillExcept(strukt interface{}, fields ...string) {
+	fillWithDetails(strukt, []string{}, fields)
+}
+
+func fillWithDetails(strukt interface{}, only []string, except []string) {
+	elem := reflect.ValueOf(strukt).Elem()
+	elemT := reflect.TypeOf(strukt).Elem()
+
+	for i := 0; i < elem.NumField(); i++ {
+
+		field := elem.Field(i)
+		fieldt := elemT.Field(i)
+		fakeType := fieldt.Tag.Get("fako")
+
+		inOnly := len(only) == 0 || (len(only) > 0 && contains(only, fieldt.Name))
+		notInExcept := len(except) == 0 || (len(except) > 0 && !contains(except, fieldt.Name))
+
+		if field.CanSet() && fakeType != "" && inOnly && notInExcept {
+			for kind, function := range typeMapping {
+				if fakeType == kind {
+					field.SetString(function())
+					break
+				}
+			}
+		}
+	}
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
---
+++ fako_test.go
@@ -0,0 +1,48 @@
+package fako
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/wawandco/fako/Godeps/_workspace/src/github.com/stretchr/testify/assert"
+)
+
+type OU struct {
+	Name     string `fako:"name"`
+	Username string `fako:"username"`
+	Email    string `fako:"email"`
+	Phone    string `fako:"phone"`
+	Password string `fako:"password"`
+	Address  string `fako:"address"`
+}
+
+func TestFillsFields(t *testing.T) {
+	var myCase OU
+	Fill(&myCase)
+	var first = myCase.Name
+	assert.NotEqual(t, myCase.Name, "")
+	Fill(&myCase)
+	assert.NotEqual(t, myCase.Name, first)
+}
+
+func TestFillsEmail(t *testing.T) {
+	var myCase OU
+	Fill(&myCase)
+
+	assert.True(t, strings.Contains(myCase.Email, "@"))
+}
+
+func TestFillOnly(t *testing.T) {
+	var myCase OU
+	FillOnly(&myCase, "Name")
+
+	assert.Equal(t, myCase.Email, "")
+}
+
+func TestFillExcept(t *testing.T) {
+	var myCase OU
+	FillExcept(&myCase, "Name")
+
+	assert.Equal(t, myCase.Name, "")
+	assert.NotEqual(t, myCase.Email, "")
+}
