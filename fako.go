93d729c75edf76b268969a64571cf5f35b11990f
Benjamin jones <ben@Benjamins-MacBook-Pro.local>
2016-06-04 16:30:49 -0700

    removed commented out logging statement

--- fako.go
+++ fako.go
@@ -85,7 +85,6 @@ var customGenerators = map[string]func() string{}
 //Fill fill all the fields that have a fako: tag
 func Fill(elems ...interface{}) {
 	for _, elem := range elems {
-		//log.Println(elem)
 		FillElem(elem)
 	}
 }
